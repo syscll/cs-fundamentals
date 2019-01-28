ac33e9d30256f535a02e08ca12e4b8703991799b
Dan Bond <pm@danbond.io>
2019-01-28 15:53:05 +0000

    remove main file

--- lru.go
+++ lru.go
@@ -1,4 +1,4 @@
-package main
+package csfundamentals
 
 import (
 	"container/list"
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Implementations of common CS fundamentals in Go.")
-}
